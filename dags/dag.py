b9a3cd5057573e28eafc80b27c8d5e3cefae05a5
blessedmadukoma <blessedmadukoma@gmail.com>
2024-07-08 00:57:09 +0100

    add operators and dependencies

--- dags/dag.py
+++ dags/dag.py
@@ -31,7 +31,7 @@ dag = DAG(
 
 # tasks or functions: 
 # Task 1: fetch amazon data (extract), Task 2:  2. clean data (transform)
-def get_amazon_data_books(num_books, ti):
+def get_amazon_data_books(num_books, **kwargs):
     base_url = f"https://www.amazon.com/s?k=data+engineering+books"
 
     books = []
@@ -70,8 +70,8 @@ def get_amazon_data_books(num_books, ti):
                         book_info = {
                             "Title": title,
                             "Author": author.text.strip(),
-                            "price": price.text.strip(),
-                            "rating": rating.text.strip(),
+                            "Price": price.text.strip(),
+                            "Rating": rating.text.strip(),
                             # "date_updated": t1,
                         }
 
@@ -93,13 +93,13 @@ def get_amazon_data_books(num_books, ti):
     df.drop_duplicates(subset="Title", inplace=True)
 
     # push the DataFrame to XCom
-    ti.xcom_push(key='book_data', value=df.to_dict('records'))
+    kwargs['ti'].xcom_push(key='book_data', value=df.to_dict('records'))
 
     # return df
 
 # Task 3: create and store data in postgres database (load)
-def insert_book_data_to_postgres(ti):
-    book_data = ti.xcom_pull(key='book_data', task_ids='fetch_book_data')
+def insert_book_data_to_postgres(**kwargs):
+    book_data = kwargs['ti'].xcom_pull(key='book_data', task_ids='fetch_book_data')
 
     if not book_data:
         raise ValueError('Book data not found')
@@ -115,7 +115,36 @@ def insert_book_data_to_postgres(ti):
         postgres_hook.run(insert_query, parameters=(book['Title'], book['Author'], book['Price'], book['Rating']))
 
 # operators: PythonOperator and PostgresOperator
+fetch_books_data_task = PythonOperator(
+    task_id="fetch_book_data",
+    python_callable=get_amazon_data_books,
+    op_args=[50],  # Number of books to fetch: 50 books
+    # op_kwargs={"num_books": 50},
+    dag=dag,
+)
+
+create_table_task = PostgresOperator(
+    task_id="create_books_table",
+    postgres_conn_id="amazon_books_connection",
+    sql="""
+        CREATE TABLE IF NOT EXISTS books (
+            id SERIAL PRIMARY KEY,
+            title TEXT NOT NULL,
+            authors TEXT,
+            price TEXT,
+            rating TEXT
+        );
+    """,
+    dag=dag,
+)
+
+insert_book_data_task = PythonOperator(
+    task_id="insert_book_data",
+    python_callable=insert_book_data_to_postgres,
+    dag=dag,
+)
 
 # hooks - allow airflow connections to external dbs e.g. postgres
 
 # dependencies: hierarchy in which tasks or functions are performed
+fetch_books_data_task >> create_table_task >> insert_book_data_task
